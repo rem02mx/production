3d4639e282e4b689cca44d0618ce446cf9cf2d90
rem02mx <r.14xoxo@gmail.com>
2017-03-31 04:59:00 +0900

    Update twiRanks.r

--- TwitterUserAnalysis/twiRanks.r
+++ TwitterUserAnalysis/twiRanks.r
@@ -1,7 +1,12 @@
+#ツイート単語と頻出回数のみのデータフレームを作成しその中で上位10単語以内を抽出
+#(解析にかけているユーザのアカウントIDがトップに来てしまうためそれは覗いて抽出している)
 rank_table <- data.frame(word=as.character(twi_table[c(-1),1]),freq=twi_table[c(-1),4])
 rank_table <- subset(rank_table, rank(-freq)<10)
 
+#グラフ作成のライブラリを読み込む
 library(ggplot2)
+
+#グラフ作成
 rankPlot <-
  ggplot(rank_table, aes(x=reorder(word,freq*-1), y=freq)) +
    geom_bar(stat = "identity", fill="#b4ebfa",  width = 0.5,) +
@@ -10,4 +15,6 @@ rankPlot <-
     xlab("単語")+
     ylab("頻度(回)")+
     theme(legend.position="none",text = element_text(size=4, family = "Osaka"))
+
+#作成したグラフをpng形式で保存
 ggsave(file = "rank.png", plot = rankPlot, dpi = 700, width = 3, height = 3)
