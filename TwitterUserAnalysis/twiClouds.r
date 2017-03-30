5cdfe3280d560a4ba71dcd2bd4b08f9e2982b3f6
rem02mx <r.14xoxo@gmail.com>
2017-03-31 05:27:33 +0900

    Update twiClouds.r

--- TwitterUserAnalysis/twiClouds.r
+++ TwitterUserAnalysis/twiClouds.r
@@ -1,8 +1,17 @@
+#ワードクラウドを保存するためのpng作成
 png("cloud.png")
+
+#下処理したデータの頻出回数上位100単語を抽出
 cloud_table <- subset(twi_table, rank(-Freq)<100)
 
+#ワードクラウドを作成するためのライブラリを読み込む
 library( wordcloud )
+
+#カラーパレットを使用するためのライブラリを読み込む
 library( RColorBrewer )
+
+#ワードクラウドを作成し保存
 par(family = "Osaka")
-wordcloud( cloud_table[c(-1),1],cloud_table[c(-1),4], scale = c( 6, 1 ), min.freq = 1 ,max.words = Inf, random.order = FALSE, rot.per =.05,colors = brewer.pal(9, "Blues"))
+wordcloud( cloud_table[c(-1),1],cloud_table[c(-1),4], scale = c( 6, 1 ), min.freq = 1 ,
+          max.words = Inf, random.order = FALSE, rot.per =.05,colors = brewer.pal(9, "Blues"))
  dev.off()
