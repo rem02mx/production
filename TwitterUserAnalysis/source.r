1edca22ab7c41b7dbcf56a8cb0d137713afb0bbf
rem02mx <r.14xoxo@gmail.com>
2017-03-31 03:54:09 +0900

    Update source.r

--- TwitterUserAnalysis/source.r
+++ TwitterUserAnalysis/source.r
@@ -1,8 +1,13 @@
 source('~/Desktop/TwitterUserAnalysis/setup.R')
 
 #手打ち
+#解析するTwitterのアカウントID
 userID <- "pyyyming"
+
+#ツイート読み込み
 tldata <- userTimeline(userID,n=1000,maxID=NULL,sinceID=NULL,includeRts=FALSE,excludeReplies=FALSE)
+
+#読み込んだツイートをRのデータとして書き出し
 sink( file = "tweetData.R" )
 tldata
 sink()
