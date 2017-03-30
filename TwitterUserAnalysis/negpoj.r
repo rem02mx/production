bcc193282d6cc01572495d127977ca7e86fdbbf0
rem02mx <r.14xoxo@gmail.com>
2017-03-31 05:34:15 +0900

    Update negpoj.r

--- TwitterUserAnalysis/negpoj.r
+++ TwitterUserAnalysis/negpoj.r
@@ -1,19 +1,38 @@
+#指定アカウントのツイートの,　ネガティブワード・ポジティブワードの""数""を表したグラフを生成する
+#ポジティブワード・ネガティブワードの""割合""を円グラフにするものは作成中
+
+#単語感情極性表を読み込む
 pndic <- read.table("http://www.lr.pi.titech.ac.jp/~takamura/pubs/pn_ja.dic",
                      sep=":",
                      col.names = c("term","kana","pos","value"),
                      colClasses=c("character","character","factor","numeric"),
                      fileEncoding = "SJIS")
+
+#名詞＋品詞で複数の候補がある場合は平均値を採用
 pndic2 <- aggregate(value ~ term + pos,pndic,mean)
+
+#下処理したデータから単語感情極性表に含まれる単語を抽出
 tweetDF<-subset(twi_table,Term %in% pndic2$term)
+
+#単語感情極性表の属性をマージ
 tweetDF<-merge(tweetDF, pndic2,by.x = c("Term","Info1"),by.y=c("term","pos"))
+
+#単語毎にスコア算出
 score<-tweetDF[4:(ncol(tweetDF) - 2)]*pndic2$value
 
+#描画用データを作成
 ans <-c(sum(score > 0),sum(score < 0))
+
+#グラフ作成のライブラリを読み込む
 library(ggplot2)
+
+#グラフ用データフレームを作成
 ansDF <- data.frame(
   emotion = c("positive", "negative"),
   value = ans
   )
+
+#グラフ作成
 emoPlot <-  ggplot(ansDF) +
    geom_bar(aes(x=emotion, y= value, fill=emotion), width = 0.5, stat="identity", position="dodge") +
    geom_text(aes(x= emotion, y= value, label=value),size=1.5, position="stack",vjust = 3) +
@@ -22,4 +41,6 @@ emoPlot <-  ggplot(ansDF) +
    theme_minimal()+
    scale_fill_brewer(palette="Blues")+
    theme(legend.position="none",text = element_text(size=4, family = "Osaka"))
-   ggsave(file = "emotion.png", plot = emoPlot, dpi = 700, width = 3, height = 2)
+
+#作成したグラフをpng形式で保存
+ggsave(file = "emotion.png", plot = emoPlot, dpi = 700, width = 3, height = 2)
